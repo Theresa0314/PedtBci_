3b581c37d4beb94a2a9d0617e5c7dad0e4c354ee
Theresa0314 <theresa_k_sy@dlsu.edu.ph>
2023-11-14 15:26:30 +0800

    Updated Referral details

--- src/pages/referral/referraldetails.jsx
+++ src/pages/referral/referraldetails.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase.config';
 import {
-  Box, Typography, CircularProgress, Container, Card, CardContent, Divider, useTheme
+  Box, Typography, CircularProgress, Container, Card, CardContent, Divider, 
+  useTheme, Paper, Grid, Tab, Tabs
 } from '@mui/material';
 import Header from '../../components/Header';
 import { tokens } from '../../theme';
@@ -11,25 +12,55 @@ import { tokens } from '../../theme';
 const ReferralDetails = () => {
   const { referralId } = useParams(); // Use the referral ID from the URL
   const [referralData, setReferralData] = useState(null);
+  const [patientData, setPatientData] = useState(null);
+  const [currentTab, setCurrentTab] = useState(0); // Added state for current tab
   const [loading, setLoading] = useState(true);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  useEffect(() => {
-    const fetchReferralData = async () => {
-      const docRef = doc(db, "referralform", referralId);
-      const docSnap = await getDoc(docRef);
+  const handleTabChange = (event, newValue) => {
+    setCurrentTab(newValue);
+  };
 
-      if (docSnap.exists()) {
-        setReferralData(docSnap.data());
+  useEffect(() => {
+    const fetchReferralAndPatientData = async () => {
+      // Fetch the referral data
+      const referralDocRef = doc(db, "referralform", referralId);
+      const referralDocSnap = await getDoc(referralDocRef);
+  
+      if (referralDocSnap.exists()) {
+        const referralData = referralDocSnap.data();
+        setReferralData(referralData);
+        
+        // Use the caseNumber from the referral data to fetch the patient data
+        const caseNumber = referralData.caseNumber;
+        const patientQuery = query(collection(db, "patientsinfo"), where("caseNumber", "==", caseNumber));
+        const patientQuerySnapshot = await getDocs(patientQuery);
+        
+        if (!patientQuerySnapshot.empty) {
+          // Assuming caseNumber is unique and there's only one matching patient document
+          const patientData = patientQuerySnapshot.docs[0].data();
+          setPatientData(patientData);
+        } else {
+          console.log("No such patient!");
+        }
       } else {
         console.log("No such referral!");
       }
       setLoading(false);
     };
-
-    fetchReferralData();
+  
+    fetchReferralAndPatientData();
   }, [referralId]);
+  
+
+    // Function to calculate age from date of birth
+    const calculateAge = (dob) => {
+        const birthDate = new Date(dob);
+        const difference = Date.now() - birthDate.getTime();
+        const ageDate = new Date(difference);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+      };
 
   if (loading) {
     return (
@@ -40,24 +71,66 @@ const ReferralDetails = () => {
   }
 
   return (
-    <Container sx={{ mt: 4 }}>
-      <Header title="Referral Details" />
-      {referralData ? (
-        <Card raised sx={{ backgroundColor: colors.primary[400], mb: 4 }}>
+    <Box padding={3} bgcolor="background.default" color="text.primary">
+        <Header title="Referral Information" subtitle="Managing referral data" />
+        <Tabs
+          value={currentTab}
+          onChange={handleTabChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+        >
+        <Tab label="Patient Info" />
+        <Tab label="Referral Info" />
+        <Tab label="Receiving Info" />
+      </Tabs>
+
+      {referralData && patientData ? (
+        <Container
+        component={Paper}
+        elevation={3}
+        sx={{
+        padding: theme.spacing(3),
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+        backgroundColor: colors.primary[400],
+        maxWidth: 'none', 
+        }}
+        >          
+       {/* Personal Information */}
+       {currentTab === 0 && (
+        //<Card raised sx={{ backgroundColor: colors.primary[400], mb: 4 }}>
           <CardContent>
-            <Typography variant="h5" gutterBottom sx={{ color: colors.greenAccent[500], fontWeight: 'bold' }}>
-              Referral Information
+          <Typography variant="h6" gutterBottom component="div" sx={{ fontWeight: 'bold', color: colors.greenAccent[500], fontSize: '1.25rem' }}>
+              Patient Information
             </Typography>
             <Divider sx={{ bgcolor: colors.grey[500], my: 2 }} />
-            {/* Display referral details here */}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body1" sx={{ fontSize: '1.1rem', marginBottom: '0.5rem' }}><strong>Full Name:</strong> {patientData.fullName}</Typography>
+            <Typography variant="body1" sx={{ fontSize: '1.1rem', marginBottom: '0.5rem' }}><strong>Age:</strong> {calculateAge(patientData.birthdate)}</Typography>
+            <Typography variant="body1" sx={{ fontSize: '1.1rem', marginBottom: '0.5rem' }}><strong>Gender:</strong> {patientData.gender}</Typography>
+            </Grid>
+         
+             <Grid item xs={12} sm={6}>
+            <Typography variant="body1" sx={{ fontSize: '1.1rem', marginBottom: '0.5rem' }}><strong>Weight:</strong> {patientData.weight} kg</Typography>
+            <Typography variant="body1" sx={{ fontSize: '1.1rem', marginBottom: '0.5rem' }}>
+              <strong>Address:</strong> {`${patientData.houseNameBlockStreet}, ${patientData.barangay}, ${patientData.city}, ${patientData.province}`}
+            </Typography>
+            </Grid>
+            </Grid>
           </CardContent>
-        </Card>
+       // </Card>
+         )}
+       {/* Referring Facility */}
+
+        </Container>
       ) : (
         <Typography variant="h6" sx={{ color: colors.redAccent[500] }}>
-          No referral information available.
+          No referral or patient information available.
         </Typography>
       )}
-    </Container>
+    </Box>
   );
 };
 
